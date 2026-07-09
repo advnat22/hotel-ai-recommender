0bd9c0c0a8716af9ddfe1d7f25df7b267f89d1d1
advnat22 <advaithn22@gmail.com>
2026-07-09 16:01:50 +0530

    Fix import path for Login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from "./pages/Login";
+import Login from "./pages/login";
 import Preferences from "./pages/Preferences";
 import Search from "./pages/Search";
 import HotelResults from "./pages/HotelResults";
