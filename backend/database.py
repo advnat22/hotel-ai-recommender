10f15d2db532cd35dc39880813c1a2e17dbd9de3
Advaith Natarajan <advaithn22@gmail.com>
2026-07-09 17:26:38 +0530

    Restructure backend for Vercel

--- backend/database.py
+++ backend/database.py
@@ -1,26 +1,28 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
+from pathlib import Path
 
-DATABASE_URL="sqlite:///./hotel.db"
+BASE_DIR = Path(__file__).parent
 
-engine=create_engine(
+DATABASE_URL = f"sqlite:///{BASE_DIR / 'hotel.db'}"
+
+engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread":False}
+    connect_args={"check_same_thread": False}
 )
 
-SessionLocal=sessionmaker(
+SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
-Base=declarative_base()
+Base = declarative_base()
 
 
 def get_db():
-
-    db=SessionLocal()
+    db = SessionLocal()
 
     try:
         yield db
